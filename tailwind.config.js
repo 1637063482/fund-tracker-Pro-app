7987c938cf0e39df4f47fea1940bf5b9c91ca04b
unknown <6207>
2026-04-20 12:47:04 +0800

    优化多终端显示

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // 【关键】：确保 Tailwind 能扫描到 App.jsx 中的类名
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animate") // 【关键修复】：使界面的 fade-in, slide-in 动画生效
+  ],
 }
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 【核心修复】：指定为相对路径，确保 WebView 和 PWA 能正确加载 assets 下的 JS/CSS 资源
   plugins: [react()],
-  // 【核心修复】强制使用相对路径寻址，防止移动端 WebView 及静态托管寻址失败
-  base: './', 
 })
