68bf42c01bffb2e1acc7ae4ac1081c26aba71624
unknown <6207>
2026-04-20 15:24:45 +0800

    修复开屏问题

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
-// tailwind.config.js
 import tailwindcssAnimate from 'tailwindcss-animate';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    tailwindcssAnimate // 【关键修复】：使用 ESM import 语法，防止构建时崩溃
+    tailwindcssAnimate
   ],
 }
